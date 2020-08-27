cb41407f4c2656c0b522589db2b2bff9b2f460aa
Aaron Brady <aleebrady@gmail.com>
2020-08-27 19:13:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,7 +54,13 @@ class Student
   end
   
   def self.find_by_name(name)
+    sql = <<-SQL
+    Select *From students Where name = ? Limit 1
+    SQL
     
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
 end
